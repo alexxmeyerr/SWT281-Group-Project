50be8f0af28f76aa0c242fcc2349b6fe1b294dc3
enzak <enzak@ENZA-LT>
2026-08-07 14:45:14 +0200

    Add project files.

---
+++ TarmacControl/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarmacControl.UI;
+
+namespace TarmacControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //set the program to still keep running. So the exit option was not selected
+            bool isRunning = true;
+
+            //while the program is still running display the menu
+            while (isRunning)
+            {
+                //call the method that will display the menu
+                DisplayDefaultMenu();
+
+                //ask the user to select the option
+                Console.WriteLine("\nInput the number that best described the action you want to perform.\n");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int choice) && Enum.IsDefined(typeof(MenuOption), choice))
+                {
+                    MenuOption selected = (MenuOption)choice;
+                    ManageSelection(selected);
+                }
+            }
+        }
+        //set a method that will display the default menu
+        static void DisplayDefaultMenu()
+        {
+            //clear the console of any remaining info
+            Console.Clear();
+
+            //Start writing the prompt and info for the menu
+            Console.WriteLine("===================== Welcome to TarmacControl =====================");
+            
+
+            //Display the menu
+            foreach(MenuOption option in Enum.GetValues(typeof(MenuOption)))
+            {
+                Console.WriteLine($"{(int)option}. {option.GetDescription()}"); //call the get description method
+            }
+        }
+
+        static void ManageSelection(MenuOption option)
+            {
+                switch (option)
+                {
+                    case MenuOption.ViewAllAircraft:
+                        //<< insert appropriate code >>
+                        break;
+
+                    case MenuOption.ViewAllVehicles:
+                        //<< insert appropriate code >>
+                    break;
+
+                    case MenuOption.RegisterAircraft:
+                        //<< insert appropriate code >>
+                        break;
+
+                    case MenuOption.DispatchVehicle:
+                        //<< insert appropriate code >>
+                        break;
+
+                    case MenuOption.AssignAircraft:
+                        //<< insert appropriate code >>
+                        break;
+
+                    case MenuOption.ViewEventLog:
+                        //<< insert appropriate code >>
+                        break;
+
+                    case MenuOption.SaveState:
+                        //<< insert appropriate code >>
+                        break;
+
+                    case MenuOption.LoadState:
+                        //insert display of all aircrafts
+                        break;
+
+                    case MenuOption.Exit:
+                        //insert display of all aircrafts
+                        break;
+
+                    default:
+                        Console.WriteLine("The option that you have selected is not valid");
+                        break;
+            }
+            }
+    }
+}
---
+++ TarmacControl/UI/ConsoleMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+//add enums that will hold the menu values for each different menu
+enum MenuOption
+{
+    [Description("View all aircrafts")] //set descriptions for each value in the enum so that we can display sentences
+    ViewAllAircraft = 1, //set the first value to have an integer value of 1
+    [Description("View all ground vehicles")]
+    ViewAllVehicles,
+    [Description("Register new flight (creates Aircraft)")]
+    RegisterAircraft,
+    [Description("Dispatch a ground vehicle to an aircraft")]
+    DispatchVehicle,
+    [Description("Assign aircraft to gate/runway")]
+    AssignAircraft,
+    [Description("View system event log")]
+    ViewEventLog,
+    [Description("Save system state")]
+    SaveState,
+    [Description("Load system state")]
+    LoadState,
+    [Description("Exit")]
+    Exit
+}
+
+namespace TarmacControl.UI
+{
+    static class ConsoleMenu
+    {
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+            return attribute?.Description ?? value.ToString();
+        }
+    }
+
+}
